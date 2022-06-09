87e46c218f0461d43018f4c54568e2d74ec646c7
jpromanonet <programming@jpromano.net>
2022-06-09 19:37:13 -0300

    Variables en ES6

---
+++ Clase_02/variables.js
@@ -0,0 +1,19 @@
+// Esto es lo que no se usa
+var primeraVariable = 1;
+
+// Esto es lo que debemos hacer
+
+// Let
+
+let nombre = "El Diego";
+let numero = 0;
+
+console.log(nombre);
+console.log(numero);
+
+// Const
+const otroNombre = "Demian";
+const nombreTutor = nombre;
+
+console.log(otroNombre);
+console.log(nombreTutor);
