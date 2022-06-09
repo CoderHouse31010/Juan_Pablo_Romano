42b4d3a4553e7b8f2ecee8cb6813f880d1db5e2f
jpromanonet <programming@jpromano.net>
2022-06-09 20:16:59 -0300

    Scopes

---
+++ Clase_02/funciones.js
@@ -0,0 +1,15 @@
+// Funciones auto-invocadas o IIFEs
+ /*
+(function(){
+    console.log("Hola la 31010!!!!");
+})();*/
+
+//var nombre = "Juan";
+
+(function ejemplo(){
+    let nombre = "Pilar";
+    console.log(nombre);
+})();
+
+//ejemplo();
+//console.log(ejemplo());
