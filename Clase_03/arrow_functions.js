23cd9e2d8b783b8c45d6754ae508833d00c43632
jpromanonet <programming@jpromano.net>
2022-06-13 20:02:07 -0300

    Funciones en JS

---
+++ Clase_03/arrow_functions.js
@@ -0,0 +1,16 @@
+// Funcion clasica con una constante.
+const hola = function() {
+    return "Buenas buenas a la Diegoneta!! =)";
+}
+
+console.log(hola());
+
+// Como declarar la mitica arrow function!
+/*const hola2 = () => {
+    return "Buenas buenas a la Diegoneta de Arrow Functions!"
+}*/
+
+// Vamo' a simplificar =)
+const hola2 = () => "Buenas buenas a la Diegoneta de Arrow Functions!"
+
+console.log(hola2());
