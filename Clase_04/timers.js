bceaf41b8b47ddca0ac1543bbd02a005b6069b48
jpromanonet <programming@jpromano.net>
2022-06-15 19:54:37 -0300

    setInterval

---
+++ Clase_04/timers.js
@@ -0,0 +1,9 @@
+// Timeout
+function miSaludo(){
+    console.log("Buenas buenas comision 31010");
+}
+
+const miTiempo = setTimeout(miSaludo, 5000);
+
+// Set Interval
+setInterval(function () { console.log("Este es el clima!!!")}, 10000);
