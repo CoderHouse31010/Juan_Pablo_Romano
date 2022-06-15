f650e7bb1153eeb5b09cc0ef34528a81402c2da5
jpromanonet <programming@jpromano.net>
2022-06-14 21:15:19 -0300

    Async Await

---
+++ Clase_03/async.js
@@ -0,0 +1,18 @@
+// Funcion asincrona
+async function miFuncion(){
+    return "Hola!"
+}
+
+// Es lo mismo que esto
+function miFuncion2() {
+    return Promise.resolve("Hola!");
+}
+
+// Como lo usariamos en una promesa
+miFuncion2().then(
+    function(value){return "Exito"},
+    function(error){return "Error"}
+);
+
+// Como usarlo con async
+let respuesta = await miFuncion;
