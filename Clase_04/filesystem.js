abebc2f6f5b6da44e1931948cf25b2b0abf35cb8
jpromanonet <programming@jpromano.net>
2022-06-16 20:54:11 -0300

    Node filesystem

---
+++ Clase_04/filesystem.js
@@ -0,0 +1,33 @@
+var fs = require('fs');
+
+// Crear un archivo
+fs.appendFileSync('primerTexto.txt', 'Hola Comision 31010!!!\n', function(err) {
+    if (err) throw err;
+    console.log("Guardado!");
+});
+
+// Escribir un archivo sin contenido
+fs.open('./segundoTexto.txt', 'w', function(err, file){
+    if (err) throw err;
+    console.log("Guardado!")
+})
+
+// Crear un archivo usando write
+fs.writeFileSync('tercerTexto.txt', 'Hola contenido =)', function(err){
+    if (err) throw err;
+    console.log("Guardado!!!");
+})
+
+// Vamos a modificar un archivo con append
+fs.appendFileSync('./primerTexto.txt', 'Son las 20:48', function(err){
+    if (err) throw err;
+    console.log("Actualizado =)")
+})
+
+// Otra forma de modificar un archivo es con write
+fs.writeFileSync('./segundoTexto.txt', 'Esta es la primer linea!', function(err){
+    if (err) throw err;
+    console.log("El archivo ha sido modificado con exito!");
+});
+
+// Esta es la for
