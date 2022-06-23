1074920ddb3e66cf9b2da5677ae2f92c6dc047ef
jpromanonet <programming@jpromano.net>
2022-06-23 20:10:50 -0300

    Server basico en Node con express

---
+++ Clase_06/server2.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hola comision 31010!!');
+})
+
+app.listen(port, () => {
+    console.log("Tu servidor esta corriendo en el puerto: " + port);
+})
+
+app.on("error", error => console.log("El error es: " + error));
